aa6b89a3ff1b47b01d566dd55dab9cb315ffc50f
giji676 <tvtengoxachidze@gmail.com>
2025-04-27 00:23:56 +0100

    added matSelector

--- matSelector.py
+++ matSelector.py
@@ -1,3 +1,4 @@
+import os
 import maya.cmds as cmds
 import maya.api.OpenMaya as om
 
@@ -6,17 +7,164 @@ class MatSelector(om.MPxCommand):
 
     def __init__(self):
         om.MPxCommand.__init__(self)
+        self.matSelector_last_folder = "matSelector_last_folder"
+        self.lib_path = ""
+
+        self.width, self.height = 400, 500
+
+        self.window = None
+        self.lyt_scroll = None
+        self.inp_search_field = None
+        self.lyt_grid = None
+
+        self.mat_list = {}
+        self.sorted_mats = []
 
     @staticmethod
     def cmdCreator():
         return MatSelector()
 
     def run(self):
-        print("mat selector run")
+        self.lib_path = self.load_last_path()
+        self.open_mat_selector()
+        self.refresh_directory()
 
-        if cmds.window("matSelectorWin", exists=True):
-            cmds.deleteUI("matSelectorWin")
-        cmds.window("matSelectorWin", title="Material Selector", widthHeight=(200, 100))
-        cmds.columnLayout(adjustableColumn=True)
-        cmds.text(label="Hello mat selector")
-        cmds.showWindow("matSelectorWin")
+    def refresh_directory(self, *args):
+        self.update_mat_list()
+        search_text = cmds.textField(self.inp_search_field, query=True, text=True).lower()
+        if search_text != "":
+            self.search()
+        else:
+            self.sort_mats()
+        self.display_mats()
+
+    def save_last_path(self, path):
+        cmds.optionVar(stringValue=(self.matSelector_last_folder, path))
+
+    def load_last_path(self):
+        if cmds.optionVar(exists=self.matSelector_last_folder):
+            last_folder = cmds.optionVar(q=self.matSelector_last_folder)
+            return last_folder
+        else:
+            return ""
+
+    def open_folder_selector_dialog(self, *args):
+        selected = cmds.fileDialog2(
+            caption="Select Material Folder",
+            fileMode=2,  # 2 = Select only folder
+            okCaption="Select"
+        )
+        if selected:
+            self.lib_path = selected[0]
+            self.save_last_path(self.lib_path)
+            self.update_mat_list()
+            self.sort_mats()
+            self.display_mats()
+
+    def search(self, *args):
+        search_text = cmds.textField(self.inp_search_field, query=True, text=True).lower()
+
+        matching_materials = []
+        non_matching_materials = []
+        for base_name in sorted(self.mat_list):
+            if base_name.lower().startswith(search_text):
+                matching_materials.append(base_name)
+            else:
+                non_matching_materials.append(base_name)
+
+        self.sorted_mats = matching_materials + non_matching_materials
+        self.display_mats()
+
+    def sort_mats(self):
+        self.sorted_mats = []
+        for base_name in sorted(self.mat_list):
+            self.sorted_mats.append(base_name)
+
+    def update_mat_list(self):
+        if not os.path.exists(self.lib_path):
+            cmds.inViewMessage(amg=f"Folder '{self.lib_path}' missing", pos='midCenter', fade=True)
+            return
+
+        self.mat_list = {}
+        for mat in os.listdir(self.lib_path):
+            full_path = os.path.join(self.lib_path, mat)
+            if os.path.isdir(full_path):
+                self.load_mat(mat)
+
+    def load_mat(self, mat):
+        full_mat_path = os.path.join(self.lib_path, mat)
+        if not os.path.exists(full_mat_path):
+            cmds.inViewMessage(amg=f"Missing '{full_mat_path}'. Ignoring", pos='midCenter', fade=True)
+            return
+        ma_path = os.path.join(self.lib_path, mat, mat+".ma")
+        if not os.path.exists(ma_path):
+            cmds.inViewMessage(amg=f"Missing '{ma_path}'. Ignoring", pos='midCenter', fade=True)
+            return
+        prev_path = os.path.join(self.lib_path, mat, mat+".png")
+        if not os.path.exists(ma_path):
+            cmds.inViewMessage(amg=f"Missing '{prev_path}'", pos='midCenter', fade=True)
+
+        self.mat_list[mat] = {"ma_path": ma_path, "prev_path": prev_path}
+
+    def open_mat(self, mat_path):
+        cmds.file(mat_path, i=True, ignoreVersion=True)
+
+    def display_mats(self):
+        columns = 4
+        cell_width = (self.width-5*2)/columns # include 5px padding on both sides
+        cell_height = cell_width + 30
+
+        if cmds.layout("grid", exists=True):
+            cmds.deleteUI("grid")
+
+        self.lyt_grid = cmds.gridLayout("grid", numberOfColumns=columns, cellWidth=cell_width, cellHeight=cell_height, parent=self.lyt_scroll)
+
+        for base_name in self.sorted_mats:
+            cmds.columnLayout(parent=self.lyt_grid)
+            cmds.iconTextButton(
+                image3=self.mat_list[base_name]["prev_path"],
+                style="iconOnly",
+                width=cell_width,
+                height=cell_width,
+                command=lambda ma_path=self.mat_list[base_name]["ma_path"]: self.open_mat(ma_path),
+            )
+            cmds.text(label=base_name, align="center")
+            cmds.setParent('..')
+
+    def open_mat_selector(self):
+        if cmds.window("materialSelectorWindow", exists=True):
+            cmds.deleteUI("materialSelectorWindow", window=True)
+
+        self.window = cmds.window("materialSelectorWindow", title="Material Selector", widthHeight=(self.width, self.height), sizeable=False)
+        main_layout = cmds.formLayout(parent=self.window)
+        folder_button = cmds.button(label="Select Material Lib Folder", command=self.open_folder_selector_dialog, parent=main_layout)
+
+        row_layout = cmds.rowLayout(numberOfColumns=2, adjustableColumn=1, columnAlign=(1, 'left'), columnAttach=(2, 'both', 0), parent=main_layout)
+        self.inp_search_field = cmds.textField(placeholderText="Search for material...", parent=row_layout)
+        cmds.button(label="Search", command=self.search, parent=row_layout)
+        btn_refresh = cmds.button(label="Refresh", command=self.refresh_directory, parent=main_layout)
+
+        self.lyt_scroll = cmds.scrollLayout(parent=main_layout)
+
+        cmds.formLayout(
+            main_layout, edit=True,
+            attachForm=[
+                (folder_button, 'top', 5),
+                (folder_button, 'left', 5),
+                (folder_button, 'right', 5),
+                (row_layout, 'left', 5),
+                (row_layout, 'right', 5),
+                (self.lyt_scroll, 'left', 5),
+                (self.lyt_scroll, 'right', 5),
+                (btn_refresh, 'left', 5),
+                (btn_refresh, 'right', 5),
+                (btn_refresh, 'bottom', 5)
+            ],
+            attachControl=[
+                (row_layout, 'top', 5, folder_button),
+                (self.lyt_scroll, 'top', 5, row_layout),
+                (self.lyt_scroll, 'bottom', 5, btn_refresh)
+            ]
+        )
+
+        cmds.showWindow(self.window)
--- materialsPlugin.py
+++ materialsPlugin.py
@@ -1,4 +1,5 @@
 import sys
+import importlib
 import maya.api.OpenMaya as om
 import maya.cmds as cmds
 from matSelector import MatSelector
@@ -10,6 +11,23 @@ def maya_useNewAPI():
     expects to be passed, objects created using the Maya Python API 2.0.
     """
     pass
+
+def reload_materials_plugin(*args):
+    print("reloading materialsPlugin")
+    plugin_name = "materialsPlugin.py"
+
+    if cmds.pluginInfo(plugin_name, query=True, loaded=True):
+        cmds.unloadPlugin(plugin_name)
+
+    cmds.refresh()
+    import time
+    time.sleep(0.5)
+
+    for module_name in ["matSelector", "matBuilder", "materialsPlugin"]:
+        if module_name in sys.modules:
+            del sys.modules[module_name]
+
+    cmds.loadPlugin(plugin_name)
 
 def open_material_selector(*args):
     mat_selector = MatSelector()
@@ -28,14 +46,22 @@ def initializePlugin(plugin):
             cmds.menu("MaterialsPlugin", label="Materials Plugin", parent="MayaWindow", tearOff=True)
 
         cmds.menuItem(
+            "matSelectorItem",
             label="Open Material Selector",
             parent="MaterialsPlugin",
             command=open_material_selector,
         )
         cmds.menuItem(
+            "matBuilderItem",
             label="Open Material Builder",
             parent="MaterialsPlugin",
             command=open_material_builder,
+        )
+        cmds.menuItem(
+            "reloadPluginItem",
+            label="Reload Plugin",
+            parent="MaterialsPlugin",
+            command=reload_materials_plugin,
         )
 
         plugin_fn.registerCommand(MatSelector.kPluginCmdName, MatSelector.cmdCreator)
@@ -50,6 +76,15 @@ def initializePlugin(plugin):
 def uninitializePlugin(plugin):
     plugin_fn = om.MFnPlugin(plugin)
     try:
+        if cmds.menuItem("matSelectorItem", exists=True):
+            cmds.deleteUI("matSelectorItem")
+        if cmds.menuItem("matBuilderItem", exists=True):
+            cmds.deleteUI("matBuilderItem")
+        if cmds.menuItem("reloadPluginItem", exists=True):
+            cmds.deleteUI("reloadPluginItem")
+        if cmds.menu("MaterialsPlugin", exists=True):
+            cmds.deleteUI("MaterialsPlugin", menu=True)
+
         plugin_fn.deregisterCommand(MatSelector.kPluginCmdName)
         plugin_fn.deregisterCommand(MatBuilder.kPluginCmdName)
 
